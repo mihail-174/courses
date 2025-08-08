b129aa446ffda35c843404e686bd65df661466a8
приданников михаил <pme@dextra.ru>
2025-08-08 09:37:02 +0500

    9

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import {ref, onMounted} from 'vue'
 definePageMeta({
-    title: '4 Главная'
+    title: '5 Главная'
 })
 import type {Course} from '../server/types'
 const courses = ref<Course[]>([])
